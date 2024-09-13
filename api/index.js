9202d2ec7249f8ec32f1797be138dd862754513f
Мухаммад Якубов <yakub.off@icloud.com>
2024-09-14 00:26:28 +0600

    login

--- api/index.js
+++ api/index.js
@@ -5,6 +5,22 @@ require("dotenv").config();
 
 app.use(express.json());
 
-app.listen(process.env.PORT, () =>
-  console.log("Server is running on port 5000")
+const postgre = require("../database"); // Ensure this path is correct
+
+app.post("/login", async (req, res) => {
+  try {
+    const { login, password } = req.body;
+
+    const sql = "INSERT INTO users(login, password) VALUES($1, $2) RETURNING *";
+
+    const { rows } = await postgre.query(sql, [login, password]);
+
+    res.json({ msg: "OK", user: rows[0] });
+  } catch (error) {
+    res.status(500).json({ msg: error.message });
+  }
+});
+
+app.listen(process.env.PORT || 5000, () =>
+  console.log(`Server is running on port ${process.env.PORT || 5000}`)
 );
