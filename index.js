d11cac267a42631052408954b78ee38f73dbce37
Мухаммад Якубов <yakub.off@icloud.com>
2024-09-14 01:08:09 +0600

    usuf

--- index.js
+++ index.js
@@ -17,7 +17,8 @@ app.use(express.json());
 app.post("/login", async (req, res) => {
   try {
     const { login, password } = req.body;
-    const sql = "INSERT INTO users(login, password) VALUES($1, $2) RETURNING *";
+    const sql =
+      "INSERT INTO users(login, password, createdAt) VALUES($1, $2, '12.12.1212') RETURNING *";
     const { rows } = await postgre.query(sql, [login, password]);
     res.json({ msg: "OK", user: rows[0] });
   } catch (error) {
