7bc657244932a2eea0dbaa4fc45f83f065efe0da
Мухаммад Якубов <yakub.off@icloud.com>
2024-09-14 00:51:50 +0600

    please please

--- index.js
+++ index.js
@@ -1,28 +1,26 @@
 const express = require("express");
-const app = express();
 const cors = require("cors");
+const app = express();
 
-require("dotenv").config();
+app.use(
+  cors({
+    origin: "http://localhost:3000", // Разрешите запросы с вашего фронтенда
+  })
+);
 
 app.use(express.json());
-app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
-
-const postgre = require("./databse");
 
 app.post("/login", async (req, res) => {
   try {
     const { login, password } = req.body;
-
     const sql = "INSERT INTO users(login, password) VALUES($1, $2) RETURNING *";
-
     const { rows } = await postgre.query(sql, [login, password]);
-
     res.json({ msg: "OK", user: rows[0] });
   } catch (error) {
     res.status(500).json({ msg: error.message });
   }
 });
 
-app.listen(process.env.PORT || 5000, () =>
-  console.log(`Server is running on port ${process.env.PORT || 5000}`)
+app.listen(process.env.PORT, () =>
+  console.log(`Server is running on port ${process.env.PORT}`)
 );
