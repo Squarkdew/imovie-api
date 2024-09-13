aa4f4c4bc53dc23d8fbb944d2482168a7ffdefc3
Мухаммад Якубов <yakub.off@icloud.com>
2024-09-14 00:39:31 +0600

    fix login

--- api/index.js
+++ api/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 
 require("dotenv").config();
 
 app.use(express.json());
+app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
 
 const postgre = require("../database"); // Ensure this path is correct
 
