fac64ee8ae2901e8fb2b0e371d5757e22d3ca318
Мухаммад Якубов <yakub.off@icloud.com>
2024-09-13 14:03:41 +0600

    fuck every

--- routes/commentsRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const commentsController = require("../controllers/commentsController");
-router.post("/add", commentsController.add.bind(commentsController));
-router.post("/delete", commentsController.delete.bind(commentsController));
-router.post("/change", commentsController.change.bind(commentsController));
-
-module.exports = router;
--- routes/personRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const personController = require("../controllers/personController");
-router.post("/add", personController.add.bind(personController));
-router.post("/delete", personController.delete.bind(personController));
-router.post("/get", personController.get.bind(personController));
-
-module.exports = router;
